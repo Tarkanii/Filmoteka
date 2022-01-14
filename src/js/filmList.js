167ebab0d77c50f2941737b5dd139be794e718dd
Artur Tarkanii <86352980+Tarkanii@users.noreply.github.com>
2022-01-14 21:52:44 +0200

    modal added

--- src/js/filmList.js
+++ src/js/filmList.js
@@ -2,7 +2,66 @@ import { fetchTrending, fetchGenres } from './api';
 
 const IMG_URL = 'https://image.tmdb.org/t/p/w500';
 
-const getGenreNames = async (idArr, type) => {
+document.addEventListener('DOMContentLoaded', async () => {
+  renderTrending({});
+});
+
+async function renderTrending({ page = 1, type = 'movie' }){
+  try {
+    const list = (await fetchTrending({ page, type })).results;
+    renderList({ list, type });
+  } catch (err) {
+    console.log(err.message);
+  }
+};
+
+async function renderList ({ list, type = 'movie', pageType = 'home' }){
+  const filmList = document.querySelector('.film-list');
+  try {
+    const arrOfPromises = list.map(async item => {
+      const genres = await getGenreNames(item.genre_ids, type);
+      const markup = createCardMarkup({ ...item, genres, pageType });
+      return markup;
+    });
+    const markup = (await Promise.all(arrOfPromises)).join('');
+    filmList.innerHTML = '';
+    filmList.insertAdjacentHTML('beforeend', markup);
+  } catch (err) {
+    console.log(err.message);
+  }
+};
+
+function createCardMarkup ({
+  pageType = 'library',
+  genres,
+  id,
+  poster_path,
+  title,
+  release_date,
+  name,
+  first_air_date,
+  vote_average = 0,
+}){
+  return `<li class="movie-card" >
+  <img
+    src=${poster_path? `${IMG_URL}${poster_path}`:`${IMG_URL}/wjYOUKIIOEklJJ4xbbQVRN6PRly.jpg`}
+    width=280 px
+    alt="poster" data-id=${id}
+    class="movie-poster"
+  />
+  <p class="movie-name">${title || name}</p>
+  <div class="movie-thumb">
+    ${genres && genres.length ? `<p class="movie-genres">${genreLengthController(genres,32)}</p>` : ''}
+    ${
+        `<p class="movie-date">${release_date|| first_air_date?release_date?.slice(0, 4) || first_air_date?.slice(0, 4):""}</p>`
+    }
+    ${pageType === 'library' ? `<span class="movie-rate">${vote_average.toFixed(1)}</span>` : ''}
+  </div>
+</li>`;
+};
+
+
+async function getGenreNames  (idArr, type){
   try {
     const genres = await fetchGenres(type);
     const newArr = idArr.map(id => {
@@ -16,69 +75,11 @@ const getGenreNames = async (idArr, type) => {
   }
 };
 
-const createCardMarkup = ({
-  pageType = 'library',
-  genres,
-  id,
-  poster_path,
-  title,
-  release_date,
-  name,
-  first_air_date,
-  vote_average = 0,
-}) => {
-  const genreLengthController = genres => {
-    const sliceCount = 3;
-    let string = genres.join(', ');
-    for (let i = 0; string.length > 32; i++) {
-      string = genres.slice(0, sliceCount - i).join(', ');
-    }
-    return string;
-  };
-  return `<li class="movie-card" >
-  <img
-    src=${poster_path? `${IMG_URL}${poster_path}`:`${IMG_URL}/wjYOUKIIOEklJJ4xbbQVRN6PRly.jpg`}
-    width=280 px
-    alt="poster" data-id=${id}
-    class="movie-poster"
-  />
-  <p class="movie-name">${title || name}</p>
-  <div class="movie-thumb">
-    ${genres && genres.length ? `<p class="movie-genres">${genreLengthController(genres)}</p>` : ''}
-    ${
-        `<p class="movie-date">${release_date|| first_air_date?release_date?.slice(0, 4) || first_air_date?.slice(0, 4):""}</p>`
-    }
-    ${pageType === 'library' ? `<span class="movie-rate">${vote_average.toFixed(1)}</span>` : ''}
-  </div>
-</li>`;
-};
-
-const renderList = async ({ list, type = 'movie', pageType = 'home' }) => {
-  const filmList = document.querySelector('.film-list');
-  try {
-    const arrOfPromises = list.map(async item => {
-      // console.log(item);
-      const genres = await getGenreNames(item.genre_ids, type);
-      const markup = createCardMarkup({ ...item, genres, pageType });
-      return markup;
-    });
-    const markup = (await Promise.all(arrOfPromises)).join('');
-    filmList.innerHTML = '';
-    filmList.insertAdjacentHTML('beforeend', markup);
-  } catch (err) {
-    console.log(err.message);
+export function genreLengthController (genres,limit) {
+  const sliceCount = 3;
+  let string = genres.join(', ');
+  for (let i = 0; string.length > limit; i++) {
+    string = genres.slice(0, sliceCount - i).join(', ');
   }
+  return string;
 };
-
-const renderTrending = async ({ page = 1, type = 'movie' }) => {
-  try {
-    const list = (await fetchTrending({ page, type })).results;
-    renderList({ list, type });
-  } catch (err) {
-    console.log(err.message);
-  }
-};
-
-document.addEventListener('DOMContentLoaded', async () => {
-  renderTrending({});
-});
